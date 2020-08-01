b62e4fe02e7de45adae4ece2f63790e6f564691f
Aditya <mailingadityavs@gmail.com>
2020-08-01 13:53:08 +0530

    Adding Netlify lines.

--- functions/fb/index.js
+++ functions/fb/index.js
@@ -3,8 +3,15 @@ const cheerio = require('cheerio')
 
 LINK = 'https://www.facebook.com/people/Nafisa-Khan/100051429259431'
 
-axios.get(LINK)
-    .then((response) => {
-        const $ = cheerio.load(response.data);
-        console.log($("meta[property='og:image']").attr("content"));
-    })
+exports.handler = async (event, context) => {
+    axios.get(LINK)
+        .then((response) => {
+            const $ = cheerio.load(response.data);
+            const link = $("meta[property='og:image']").attr("content");
+            console.log($("meta[property='og:image']").attr("content"));
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ link })
+            }
+        })
+}
