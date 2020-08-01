435ac876bcaf018489ce1a677e75e8eb226d38cb
Aditya <mailingadityavs@gmail.com>
2020-08-01 14:12:50 +0530

    Idek

--- functions/fb/index.js
+++ functions/fb/index.js
@@ -4,14 +4,15 @@ const cheerio = require('cheerio')
 LINK = 'https://www.facebook.com/people/Nafisa-Khan/100051429259431'
 
 exports.handler = async (event, context, callback) => {
-    axios.get(LINK)
-        .then((response) => {
-            const $ = cheerio.load(response.data);
-            const link = $("meta[property='og:image']").attr("content");
-            console.log($("meta[property='og:image']").attr("content"));
-            callback(null, {
-                statusCode: 200,
-                body: JSON.stringify({ link })
-            })
-        })
+    const response = await axios.get(LINK)
+    const $ = cheerio.load(response.data);
+
+    const link = $("meta[property='og:image']").attr("content");
+
+    console.log($("meta[property='og:image']").attr("content"));
+
+    callback(null, {
+        statusCode: 200,
+        body: link
+    });
 }
